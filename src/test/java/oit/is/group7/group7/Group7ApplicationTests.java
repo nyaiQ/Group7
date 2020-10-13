8cfa44a3bea42dc5e339dcafc8ed2eb67ed95488
nyaiQ <e1b18022@oit.ac.jp>
2020-10-13 15:37:14 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/group7/group7/Group7ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group7.group7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
