f2c2926ffe079946ededabc6b5870568c9f600f6
kznk7 <e1b18069@st.oit.ac.jp>
2020-10-13 16:59:49 +0900

    セキュリティの認証処理を実装する

---
+++ src/main/java/oit/is/group7/group7/controller/Sample31Controller.java
@@ -0,0 +1,21 @@
+package oit.is.group7.group7.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+}
